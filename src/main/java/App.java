5a9382ee4af82bb5ec5e2796ed13734f321ea3f2
Baurzhan <baurzhan.zh@gmail.com>
2020-08-08 17:41:29 -0400

    Lesson 5 - Github setup

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("Hello students");
-        ///
-        ///
-        ///
-        ///
-        ///
+
 
     }
 }
