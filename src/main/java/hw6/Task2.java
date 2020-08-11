14366a7330a94cbc31ce8b30879ca509a9acfe2e
Kostia H <cool.kostian@gmail.com>
2020-08-11 01:05:12 -0400

    hw 6 task 2

---
+++ src/main/java/hw6/Task2.java
@@ -0,0 +1,16 @@
+package hw6;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int c = sum(11,2);
+
+        System.out.println(c);
+    }
+
+    public static int sum (int a, int b){
+
+        return a + b;
+    }
+}
