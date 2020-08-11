2d1a8ef3d296afff01699625fce8b277cbb16380
kamilmaratovic <kamilmaratovic@gmail.com>
2020-08-10 21:34:20 -0400

    hw5

---
+++ src/main/java/homework5/home.java
@@ -0,0 +1,7 @@
+package homework5;
+
+public class home {
+    public static void main(String[] args) {
+        System.out.println("Hey, whats up! ");
+    }
+}
---
+++ src/main/java/homework6/HomeWork.java
@@ -0,0 +1,7 @@
+package homework6;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
