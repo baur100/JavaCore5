be3e2e0717d3f90b99cc3b36730b34f91025738b
Ulviyya2020 <67292552+Ulviyya2020@users.noreply.github.com>
2020-08-10 21:54:53 -0500

    Home Work 6

---
+++ src/main/java/lesson6/Work.java
@@ -0,0 +1,10 @@
+package lesson6;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+import java.sql.SQLOutput;
+
+public class Work {
+    public static void main(String [] args){
+        System.out.println("Hello World, Its me!");
+    }
+}
