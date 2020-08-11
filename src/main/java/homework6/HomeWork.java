07c0a0c45cfd903c0f1d540f892462c65a61cd1b
kamilmaratovic <kamilmaratovic@gmail.com>
2020-08-11 19:52:01 -0400

    hw5

--- src/main/java/homework6/HomeWork.java
+++ src/main/java/homework6/HomeWork.java
@@ -2,6 +2,38 @@ package homework6;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println();
+
+        //Task displaying
+
+        sum(5, 6);
+
+        int xx = sum1(200, 300);
+        System.out.println(xx);
+
+        boolean yy = isEven(99);
+        System.out.println(yy);
+    }
+//Task 1
+
+    public static void sum(int a, int b) {
+        int x = a + b;
+        System.out.println(x);
+    }
+//Task 2
+
+    public static int sum1(int c, int d) {
+        int result = c + d;
+        return result;
+    }
+
+    //Task 3
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
