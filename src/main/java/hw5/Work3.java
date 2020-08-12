234877cc9a9aed3de08f1682417214e02dbb5cb3
Dmitriy <pikhtelev.dmitriy@gmail.com>
2020-08-12 17:49:03 -0500

    HW5

---
+++ src/main/java/hw5/Work3.java
@@ -0,0 +1,11 @@
+package hw5;
+
+public class Work3 {
+    public static void main(String[] args) {
+        System.out.println(isEven(65));
+    }
+    public static boolean isEven(int x){
+        return x%2==0;
+    }
+}
+
