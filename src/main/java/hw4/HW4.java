605e5823a76de3ca99ba2d07e45322a5bedcdb8e
dsinj <dsinjiashvili@gmail.com>
2020-08-12 18:13:00 -0500

    Hw4

---
+++ src/main/java/hw4/HW4.java
@@ -0,0 +1,59 @@
+package hw4;
+
+public class HW4 {
+
+    public static void main(String[] args) {
+
+ //       int[] numbers = {2, 76, 9, 90, 31, 76, 6, 88, 21, 12};
+
+//        for (int v:numbers) {
+//            System.out.println(v);
+//        }
+//
+//        for (int i=0;i<numbers.length;i++){
+//            System.out.println(numbers[i]);
+//        }
+
+//        for (int v : numbers) {
+//            if (v % 2 == 0) {
+//                System.out.println(v);
+//            }
+//
+//        }
+//        for (int v : numbers) {
+//            if (v % 3 == 0) {
+//                System.out.println(v);
+//
+//            }
+//        }
+//        int max = numbers[0];
+//        for (int v : numbers) {
+//            if (v > max) {
+//                max = v;
+//
+//            }
+//        }
+//        System.out.println(max);
+
+//        int min = numbers[9];
+//        for (int v : numbers) {
+//            if (v < min) {
+//                min = v;
+//
+//            }
+//        }
+//        System.out.println(min);
+
+        int[] numbers = {10,15,12,7,88,10,6,17,23,10,10,16};
+
+        for(int v:numbers){
+            if(v==10){
+                System.out.println(v);
+            }
+        }
+    }
+}
+
+
+
+
