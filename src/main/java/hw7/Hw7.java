8f169d5ec2bb50d971a9c65315ae6f60c974220f
dsinj <dsinjiashvili@gmail.com>
2020-08-13 13:20:26 -0500

    Hw7

---
+++ src/main/java/hw7/Hw7.java
@@ -0,0 +1,53 @@
+package hw7;
+
+public class Hw7 {
+    public static void main(String[] args) {
+        int[] a1 = {2, 3, 5, 6, 7, 8};
+        System.out.println(sumOfArray(a1));
+
+        int[] a2 = {3,9,1,8,4,8,5};
+        System.out.println(a2[i]);
+
+        int[] a3 = {3,9,1,8,4,8,5};
+        System.out.println(max(a3));
+
+
+
+
+    }
+
+    public static int sumOfArray(int[] arr) {
+        int sum = 0;
+        for (int v : arr) {
+            sum += v;
+        }
+        return sum;
+
+    }
+    public static int sort(int[] arr){
+        for(int i=0; i>a2.lengh; i++){
+
+        }
+        public static int max(int[]xx){
+            int max = xx[0];
+            for (int v : xx) {
+                if(v>max){
+                    max=v;
+                }
+            }
+            return max;
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
