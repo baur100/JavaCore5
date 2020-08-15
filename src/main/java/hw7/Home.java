8c4bcce72dac589b7fd21efe3d31aa9f1658d30a
Dmitriy <pikhtelev.dmitriy@gmail.com>
2020-08-14 22:25:01 -0500

    HW 7

--- src/main/java/hw7/Home.java
+++ src/main/java/hw7/Home.java
@@ -2,10 +2,13 @@ package hw7;
 
 public class Home {
     public static void main(String[] args) {
+        int[] arr = {3, 9, 1, 8, 4, 8, 5};
+        System.out.println(min(arr));
+        System.out.println(max(arr));
 
     }
 
-    public static int max(int[] nmb) {
+    public static int min(int[] nmb) {
         int min = nmb[0];
         for (int v : nmb) {
             if (v < min) {
@@ -15,4 +18,15 @@ public class Home {
         return min;
     }
 
-}
+    public static int max(int[] nmb) {
+        int max = nmb[0];
+        for (int v : nmb) {
+            if (v > max) {
+                max = v;
+
+            }
+
+            }
+            return max;
+        }
+    }
