796bc4269847a3f361d39acfe2c6452b0553a37c
pavel <pavel.smoljaninov@yahoo.com>
2020-08-16 13:32:21 -0400

    HW8

--- src/main/java/HW8/App8.java
+++ src/main/java/HW8/App8.java
@@ -4,5 +4,15 @@ public class App8 {
 
     public static void main(String[] args) {
 
+        //Enums
+
+       Computer good = Computer.LINUX;
+       Computer medium = Computer.WINDOWS;
+       Computer[] theBest = {Computer.MAC, Computer.LINUX};
+       Monitor large = Monitor.MAC;
+       Monitor medium2 = Monitor.DELL;
+       Monitor[] set = {Monitor.DELL, Monitor.TOSHIBA, Monitor.MAC};
+
+      
     }
 }
--- src/main/java/HW8/ComputerAndMonitor.java
+++
@@ -1,8 +0,0 @@
-package HW8;
-
-public class ComputerAndMonitor {
-
-    public static void main(String[] args) {
-
-    }
-}
