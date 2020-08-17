56e06c1af7ca9881db5c4161346f9c743493ac2e
pavel <pavel.smoljaninov@yahoo.com>
2020-08-16 21:08:47 -0400

    HW9

--- src/main/java/HW9/App9.java
+++ src/main/java/HW9/App9.java
@@ -8,17 +8,20 @@ public class App9 {
         Animals bullDog = new Animals();
         bullDog.setAge(34);
         System.out.println(bullDog.getAge());
+        bullDog.printAnimalsInfo();
 
         Animals chihuaHua = new Animals("black", 12, Vaccines.FLY, Vaccines.CORONAVIRUS);
 
         Furniture table = new Furniture();
         table.setMaterial("metal");
         System.out.println(table.getMaterial());
+        table.printFurnityreinfo();
         Furniture chair = new Furniture("wood", "ooficeUse", AssemblyParts.WOOD, AssemblyParts.SCROVES);
 
         Equipment saw = new Equipment();
         saw.setPurpose("constraction");
         System.out.println(saw.getPurpose());
+        saw.printEquipmentInfo();
 
         Equipment hammer = new Equipment("makeFurniture", "Plastic", Dimentions.SMALL, Dimentions.SUPERSMALL);
 
