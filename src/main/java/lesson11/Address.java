65b064473cba1f02b8c88ffd08f989b8e7205bb7
Baurzhan <baurzhan.zh@gmail.com>
2020-08-18 23:14:55 -0400

    Lesson 11 - Inheritance, Protected, Final class, Complex class - setter checks

---
+++ src/main/java/lesson11/Address.java
@@ -0,0 +1,71 @@
+package lesson11;
+
+public class Address {
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private int zip;
+
+    public Address(String address1, String address2, String city, String state, int zip) {
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Address() {
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address1='" + address1 + '\'' +
+                ", address2='" + address2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                '}';
+    }
+}
