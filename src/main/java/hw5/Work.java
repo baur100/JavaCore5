1268dc54cff82fb9a7830bd26480865218896da2
aiazshamasov <aiazshamasov1990@gmail.com>
2020-08-20 15:09:43 -0500

    First commit test

---
+++ src/main/java/hw5/Work.java
@@ -0,0 +1,7 @@
+package hw5;
+
+public class Work {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- src/main/java/hw66/First.java
+++ src/main/java/hw66/First.java
@@ -1,7 +1,23 @@
 package hw66;
 
 public class First {
+
+    int[] arr = {2,3,5,6,7,8};
+    int sumofarray;
     public static void main(String[] args) {
-        System.out.println("Hello Aiaz");
+
+        int[] arr = {2,3,5,6,7,8};
+        int sumofarray = sum(arr);
+        System.out.println(sumofarray);
+
+    }
+
+    private static int sum(int[] arr) {
+
+        int sum = 0;
+        for(int v: arr){
+            sum+=v;
+        }
+        return sum;
     }
 }
