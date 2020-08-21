48c7fdb5a1972123c9af0924059126567294b4c8
Dmitriy <pikhtelev.dmitriy@gmail.com>
2020-08-21 18:43:51 -0500

    HW 10

--- src/main/java/hw10/App1.java
+++ src/main/java/hw10/App1.java
@@ -2,24 +2,28 @@ package hw10;
 
 public class App1 {
     public static void main(String[] args) {
-        Mammalian squirrel = new Mammalian("Squirrel", "Not dangerous", Aptitudes.JUMP);
+        Aptitudes[] aptitudes = {Aptitudes.JUMP, Aptitudes.FLY, Aptitudes.RUN};
+
+        Mammalian squirrel = new Mammalian("Squirrel", "Not dangerous", aptitudes);
+
+        squirrel.printInfo();
+        System.out.println(squirrel.getType());
+        System.out.println("******************");
+
         Aves eagle = new Aves("Eagle", "Dangerous", 40);
         Arachnids spider = new Arachnids("Spider", "Dangerous", 9);
 
-        Aptitudes [] aptitudes = {Aptitudes.JUMP, Aptitudes.FLY, Aptitudes.RUN};
-        squirrel.printInfo();
-
         System.out.println(eagle.getHazardClass());
         eagle.printInfo();
-
-        System.out.println(squirrel.getType());
-        squirrel.printInfo();
-
         spider.printInfo();
 
+        Mammalian mammalian = new Mammalian();
+        Aptitudes [] aptitudes1 = {Aptitudes.JUMP, Aptitudes.RUN};
 
-
-
+        mammalian.setAptitudes(aptitudes1);
+        mammalian.setHazardClass("Dangerous");
+        mammalian.setType("Type");
+        mammalian.printInfo();
 
 
     }
--- src/main/java/hw10/Mammalian.java
+++ src/main/java/hw10/Mammalian.java
@@ -1,15 +1,17 @@
 package hw10;
 
+import java.util.Arrays;
+
 public class Mammalian {
     private String type;
     private String hazardClass;
-    private Aptitudes aptitudes;
+    private Aptitudes[] aptitudes;
 
 
-    public Mammalian(String type, String hazardClass, Aptitudes aptitudes) {
+    public Mammalian(String type, String hazardClass, Aptitudes[] aptitudes) {
         this.type = type;
         this.hazardClass = hazardClass;
-        this.aptitudes= aptitudes;
+        this.aptitudes = aptitudes;
     }
 
     public Mammalian() {
@@ -17,14 +19,16 @@ public class Mammalian {
     }
 
     public void setType(String type) {
+        this.type = type;
 
     }
 
     public void setHazardClass(String hazardClass) {
+        this.hazardClass = hazardClass;
 
     }
 
-    public void setAptitudes(Aptitudes aptitudes) {
+    public void setAptitudes(Aptitudes[] aptitudes) {
         this.aptitudes = aptitudes;
 
     }
@@ -37,16 +41,15 @@ public class Mammalian {
         return hazardClass;
     }
 
-    public Aptitudes getAptitudes() {
+    public Aptitudes[] getAptitudes() {
         return aptitudes;
 
     }
 
     public void printInfo() {
-        System.out.println("Mammalian{" +
-                "type='" + type + '\'' +
-                ", hazardClass='" + hazardClass + '\'' +
-                ", aptitudes='" + aptitudes + '\'' +
-                '}');
+        System.out.println("Mammalian:" +
+                "\ntype=" + type +
+                "\nhazardClass=" + hazardClass +
+                "\naptitudes=" + Arrays.toString(aptitudes));
     }
 }
