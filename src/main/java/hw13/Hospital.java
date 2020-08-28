99f544d5c429714a00dfed3b880f36947b69b070
Dmitriy <pikhtelev.dmitriy@gmail.com>
2020-08-27 21:05:14 -0500

    HW 13

--- src/main/java/hw13/Hospital.java
+++ src/main/java/hw13/Hospital.java
@@ -1,8 +1,43 @@
 package hw13;
 
-public class Hospital {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
-    }
+//public class Hospital {
+//    private String hospitalName;
+//    private Address[] address;
+////    private List<Doctor> doctor = new ArrayList<>();
+////    private Map<Integer, String> rooms;
+////    private List<InsuranceCompamies> acceptedInsurances;
+//
+//    List <Doctor> doctors = new ArrayList<>();
+//
+//
+//    public List<Doctor> getDoctor() {
+////        return doctor;
+////    }
+//
+//    public Hospital(String hospitalName, Address[] address, List<Doctor> doctor, Map<Integer, String> rooms, List<InsuranceCompamies> acceptedInsurances) {
+//        this.hospitalName = hospitalName;
+//        this.address = address;
+////        this.doctor = doctor;
+//        this.rooms = rooms;
+//        this.acceptedInsurances = acceptedInsurances;
+//    }
+//
+//    public String toString() {
+//        return "Hospital{" +
+//                "hospitalName='" + hospitalName + '\'' +
+//                ", address=" + Arrays.toString(address) +
+////                ", doctor=" + doctor +
+//                ", rooms=" + rooms +
+//                ", acceptedInsurances=" + acceptedInsurances +
+//                '}';
+//    }
+//}
 
-}
+
+
+
