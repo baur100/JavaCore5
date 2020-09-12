9b8dd00eb03e87488890835e8a3b64c11c6e5278
dsinj <dsinjiashvili@gmail.com>
2020-09-12 14:45:25 -0500

    hw playlist

---
+++ src/test/java/davitTest/LoginTest.java
@@ -0,0 +1,43 @@
+package davitTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void startUp(){
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+
+
+
+    }
+    @AfterMethod
+    public void teatDown() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
+    }
+    @Test
+    public void loginTest(){
+        loginPage loginPage = new loginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login("koeluser06@testpro.io","te$t$tudent");
+        Assert.assertTrue(mainPage.isLogoutButton());
+
+    }
+    @Test
+    public void wrongLoginTest(){
+        loginPage loginPage = new loginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login("koeluser06@testpro.io","77777");
+        mainPage.createPlaylist("Dat");
+
+    }
+}
