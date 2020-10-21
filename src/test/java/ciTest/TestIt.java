6e2d57a467af7c35e04458cfd46a23d3f649a699
baur <baurzhan.zh@gmail.com>
2020-10-20 22:09:26 -0400

    QA-2525 - Dummy test done

---
+++ src/test/java/ciTest/TestIt.java
@@ -0,0 +1,11 @@
+package ciTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestIt {
+    @Test
+    public void testIt(){
+        Assert.assertTrue(true);
+    }
+}
